de160e3636023f0ef341bc8887afbf81c8331706
s7s3 <hoh2012hoh@gmail.com>
2022-06-08 10:42:30 +0300

    first commit

---
+++ mathdojo.py
@@ -0,0 +1,18 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        self.result += num
+        for num in nums:
+            self.result += num
+        return self
+    def subtract(self, num, *nums):
+        self.result -= num
+        for num in nums:
+            self.result -= num
+        return self
+# create an instance:
+md = MathDojo()
+# to test:
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x)
